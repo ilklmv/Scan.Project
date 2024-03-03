c9e71b0c0878aef1ef3805291433a442eeb0bea5
ilklmv <ilklmv21@gmail.com>
2024-03-03 12:53:08 +0300

    Created about component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 import './App.scss';
+import About from './components/Main/about/about';
 
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <About />
       <Footer />
     </div>
   );
