3970414d2e496fad30192c2c45bc0a8c557e17ed
Ilia Klimov <ilklmv21@gmail.com>
2024-03-19 21:54:36 +0300

    edited component tariffs

--- src/components/Main/tariff/tariffs.tsx
+++ src/components/Main/tariff/tariffs.tsx
@@ -15,26 +15,60 @@ const Tariffs: React.FC = () => {
                         </div>
                         <div className="choose-information">
                             <button className="tariff-btn">Текущий тариф</button>
-                            <p className="value">799 &#8381;</p>
-                            <p>или 150 &#8381;/мес. при рассрочке на 24 мес.</p>
-                            <p>В тариф входит:</p>
+                            <div className="price">
+                                <p className="value">799 &#8381;</p>
+                                <p className="value-through">1200 &#8381;</p>
+                            </div>
+                            <p className="installment">или 150 &#8381;/мес. при рассрочке на 24 мес.</p>
+                            <p className="header-tariff-list">В тариф входит:</p>
                             <ul className="tariff-list">
                                 <li>Безлимитная история запросов</li>
                                 <li>Безопасная сделка</li>
                                 <li>Поддержка 24/7</li>
                             </ul>
-                            <button className="account-btn">Перейти в личный кабинет</button>
+                            <button className="requestData">Перейти в личный кабинет</button>
                         </div>
                     </div>
-                    <div className="choose-pro">
-                        <p className="header-block">Pro</p>
-                        <p className="text-block">Для HR и фрилансеров</p>
-                        <img className="img-block" src={process.env.PUBLIC_URL + '/img/target.svg'} alt="Лампочка" />
+                    <div className="choose-block-container">
+                        <div className="choose-pro">
+                            <p className="header-block">Pro</p>
+                            <p className="text-block">Для HR и фрилансеров</p>
+                            <img className="img-block" src={process.env.PUBLIC_URL + '/img/target.svg'} alt="Лампочка" />
+                        </div>
+                        <div className="choose-information">
+                            <div className="price">
+                                <p className="value">1299 &#8381;</p>
+                                <p className="value-through">2600 &#8381;</p>
+                            </div>
+                            <p className="installment">или 279 &#8381;/мес. при рассрочке на 24 мес.</p>
+                            <p className="header-tariff-list">В тариф входит:</p>
+                            <ul className="tariff-list">
+                                <li>Все пункты тарифа Beginner</li>
+                                <li>Экспорт истории</li>
+                                <li>Рекомендации по приоритетам</li>
+                            </ul>
+                            <button className="requestData">Подробнее</button>
+                        </div>
                     </div>
-                    <div className="choose-business">
-                        <p className="header-block">Business</p>
-                        <p className="text-block">Для корпоративных клиентов</p>
-                        <img className="img-block" src={process.env.PUBLIC_URL + '/img/phone.svg'} alt="Лампочка" />
+                    <div className="choose-block-container">
+                        <div className="choose-business">
+                            <p className="header-block">Business</p>
+                            <p className="text-block">Для корпоративных клиентов</p>
+                            <img className="img-block" src={process.env.PUBLIC_URL + '/img/phone.svg'} alt="Лампочка" />
+                        </div>
+                        <div className="choose-information">
+                            <div className="price">
+                                <p className="value">2379 &#8381;</p>
+                                <p className="value-through">3700 &#8381;</p>
+                            </div>
+                            <p className="header-tariff-list">В тариф входит:</p>
+                            <ul className="tariff-list">
+                                <li>Все пункты тарифа Pro</li>
+                                <li>Безлимитное количество запросов</li>
+                                <li>Приоритетная поддержка</li>
+                            </ul>
+                            <button className="requestData">Подробнее</button>
+                        </div>
                     </div>
                 </div>
             </div>
